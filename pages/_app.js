24bbfab8d8936080bd4ba43cd8f28e79b6865a9c
SorinNita <sorin.nita.ciurea@gmail.com>
2023-08-06 15:09:39 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,7 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
-import TagManager from 'react-gtm-module';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-      TagManager.initialize({ gtmId: 'GTM-WZB2RR6L' });
-  }, []);
   return (
     <>
       <span className="theme-bejamas" />
